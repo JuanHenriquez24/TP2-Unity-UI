70c0aa8ddc8ffc9b1c2164aeef1fca26adbf11ea
Juan Henriquez <88837385+JuanHenriquez24@users.noreply.github.com>
2022-04-21 09:00:31 -0300

    Initial commit

---
+++ Assets/Scripts/ValidarContraseña.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContraseña : MonoBehaviour
+{
+    string contraseniacorrecta;
+    string contraseniausuario;
+    public Text ingresousuario;
+    public GameObject cartelitoMsj;
+    public Text textomsj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cartelitoMsj.SetActive(false);
+        contraseniacorrecta = "12345";   
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void validarContraseña()
+    {
+        contraseniausuario = ingresousuario.text;
+        if (contraseniausuario == contraseniacorrecta)
+        {
+            cartelitoMsj.SetActive(true);
+            textomsj.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelitoMsj.SetActive(true);
+            textomsj.text = "Contraseña incorrecta";
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
